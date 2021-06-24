651d252a1beb90226dd76fb950edf8dc0aa87611
YT-GameWorks <sinha.soham@outlook.com>
2021-06-24 18:27:30 +0530

    change to dark theme

--- ui/MainWindow.xaml.cs
+++ ui/MainWindow.xaml.cs
@@ -1,10 +1,27 @@
-﻿namespace ui
+﻿using HandyControl.Data;
+using HandyControl.Themes;
+using HandyControl.Tools;
+using System.Windows;
+using System;
+
+namespace ui
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            UpdateSkin(SkinType.Dark);
+        }
+
+        public void UpdateSkin(SkinType skin)
+        {
+            SharedResourceDictionary.SharedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(ResourceHelper.GetSkin(skin));
+            Resources.MergedDictionaries.Add(new ResourceDictionary
+            {
+                Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
+            });
         }
     }
 }
