6c0575e9a52301a0ed960fd4837f29778e0ed53f
YT-GameWorks <sinha.soham@outlook.com>
2021-06-24 19:30:40 +0530

    Attempt at make window translucent

--- ui/MainWindow.xaml.cs
+++ ui/MainWindow.xaml.cs
@@ -1,27 +1,27 @@
 ﻿using HandyControl.Data;
 using HandyControl.Themes;
 using HandyControl.Tools;
-using System.Windows;
 using System;
+using System.Windows;
 
 namespace ui
 {
-    public partial class MainWindow
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-            UpdateSkin(SkinType.Dark);
-        }
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			UpdateSkin(SkinType.Dark);
+		}
 
-        public void UpdateSkin(SkinType skin)
-        {
-            SharedResourceDictionary.SharedDictionaries.Clear();
-            Resources.MergedDictionaries.Add(ResourceHelper.GetSkin(skin));
-            Resources.MergedDictionaries.Add(new ResourceDictionary
-            {
-                Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
-            });
-        }
-    }
+		public void UpdateSkin(SkinType skin)
+		{
+			SharedResourceDictionary.SharedDictionaries.Clear();
+			Resources.MergedDictionaries.Add(ResourceHelper.GetSkin(skin));
+			Resources.MergedDictionaries.Add(new ResourceDictionary
+			{
+				Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
+			});
+		}
+	}
 }
